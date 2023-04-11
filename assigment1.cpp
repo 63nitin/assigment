c9715e19ab8f4c876be21bd30a534935d8a8df07
Nitin <114581521+63nitin@users.noreply.github.com>
2023-04-11 15:06:18 +0530

    Add files via upload

---
+++ assigment1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+// Q1 - Take 2 integer values in two variables x and y and print their product.
+int main()
+{
+    int x=20;
+    int y=10;
+    cout<<x*y;
+    return 0;
+}
